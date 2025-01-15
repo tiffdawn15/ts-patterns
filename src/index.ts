3f0946650c208d438e44b6d5a6346d8fa1f9a920
Tiffany Messer <tiffanymesser@tiffanys-mbp.mynetworksettings.com>
2025-01-15 11:27:41 -0500

    Factory and Singleton pattern

--- src/index.ts
+++ src/index.ts
@@ -16,18 +16,33 @@ interface Database<T extends BaseRecord> {
   get(id: string): T | undefined;
 }
 
-class InMemoryDatabase<T extends BaseRecord> implements Database<T>  {
-  private db: Record<string, T> = {};
 
-  public set(newValue: T): void  {
-    this.db[newValue.id] = newValue; 
-  };
-  public get(id: string): T | undefined {
-    return this.db[id];
-  }; 
-}   
+// Factory Pattern ==== used for creating databases without having the code front and center 
+function createDatabase<T extends BaseRecord>(){
+  class InMemoryDatabase implements Database<T>  {
+    private db: Record<string, T> = {};
 
-const pokemonDb = new InMemoryDatabase<Pokemon>();
-pokemonDb.set({ id: "Bulbasaur", attack: 50, defense: 50 }); 
+    static instance: InMemoryDatabase = new InMemoryDatabase();
+    private constructor() {}
+  
+    public set(newValue: T): void  {
+      this.db[newValue.id] = newValue; 
+    };
+    public get(id: string): T | undefined {
+      return this.db[id]; 
+    }; 
+  }  
+  // Return Singleton
+  // const db = new InMemoryDatabase();
+  // return db;
+  return InMemoryDatabase;
+} 
+    
 
-console.log(pokemonDb.get("Bulbasaur")); 
+const pokemonDb = createDatabase<Pokemon>();
+pokemonDb.instance.set({ id: "Bulbasaur", attack: 50, defense: 50 });
+// pokemonDb.set({ id: "Bulbasaur", attack: 50, defense: 50 });
+
+// pokemonDb.set({ id: "Bulbasaur", attack: 50, defense: 50 }); 
+
+// console.log(pokemonDb.get("Bulbasaur"));   
